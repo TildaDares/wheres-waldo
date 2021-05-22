888a0bf505eb5c3cbfddfa14e4e451f68a359873
TildaDares <mathildaudufo@gmail.com>
2021-05-22 22:18:51 +0100

    generated players model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_22_211236) do
+ActiveRecord::Schema.define(version: 2021_05_22_211758) do
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 2021_05_22_211236) do
 
   create_table "pictures", force: :cascade do |t|
     t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.integer "time"
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
