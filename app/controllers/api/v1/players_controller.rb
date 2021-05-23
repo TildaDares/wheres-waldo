d7391d8e594f817535b55c6c95324f5dc1933c33
TildaDares <mathildaudufo@gmail.com>
2021-05-23 13:21:10 +0100

    used api

---
+++ app/controllers/api/v1/players_controller.rb
@@ -0,0 +1,4 @@
+class Api::V1::PlayersController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root 'pictures#index'
+  namespace :api do
+    namespace :v1 do
+      resources :pictures, only: [:index, :show] do
+        resources :characters, only: [:index, :show]
+      end
+      resources :players, only: [:index, :create]
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
