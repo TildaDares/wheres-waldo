e2c6c3963d815a6caa9fce954336e062ce28e6d3
TildaDares <mathildaudufo@gmail.com>
2021-05-26 22:41:13 +0100

    Done with the homepage

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,6 +1,46 @@
 import React from "react";
+import waldo from "../../assets/images/waldo.png";
+import store from "../../assets/images/departmentStore.jpeg";
+import beach from "../../assets/images/onTheBeach.jpeg";
+import city from "../../assets/images/City.jpeg";
 
 const Home = () => {
-  return <div>Kome</div>;
+  return (
+    <div>
+      <nav>
+        <img src={waldo} alt="waldo" />
+        <p>Where's Waldo</p>
+      </nav>
+      <main>
+        <div class="card-grid">
+          <div class="card">
+            <img src={city} />
+            <p>City</p>
+            <div class="game-controls">
+              <a class="btn btn-success">Play</a>
+              <a class="btn btn-warning">Leaderboard</a>
+            </div>
+          </div>
+          <div class="card">
+            <img src={store} />
+            <p>Department Store</p>
+            <div class="game-controls">
+              <a class="btn btn-success">Play</a>
+              <a class="btn btn-warning">Leaderboard</a>
+            </div>
+          </div>
+          <div class="card">
+            <img src={beach} />
+            <p>On the Beach</p>
+            <div class="game-controls">
+              <a class="btn btn-success">Play</a>
+              <a class="btn btn-warning">Leaderboard</a>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 };
+
 export default Home;
