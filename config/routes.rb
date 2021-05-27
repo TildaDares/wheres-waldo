48b3eb251e4c29a0d1bf0fc76e70521642b17794
TildaDares <mathildaudufo@gmail.com>
2021-05-27 19:58:11 +0100

    Added a static controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     end
   end
 
-  root 'api/v1/pictures#index'
+  root 'static#index'
+  get '/*path' => 'static#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
