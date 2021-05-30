a912609aea38dfad89d0dceb9b27efa2429b6558
TildaDares <mathildaudufo@gmail.com>
2021-05-30 18:05:14 +0100

    populated the database with character coordinates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Picture.delete_all
+Character.delete_all
+
+data = {
+  "In Town": {
+    waldo: {name: "waldo", x_coord: 42.96, y_coord: 46.99},
+    odlaw: {name: "odlaw", x_coord: 59, y_coord: 59},
+    wenda: {name: "wenda", x_coord: 43.58, y_coord: 37.47},
+    whitebeard: {name: "whitebeard", x_coord: 65.73, y_coord: 48.13},
+  },
+
+  "Ski Slopes": {
+    waldo: {name: "waldo", x_coord: 85.5, y_coord: 45.75},
+    odlaw: {name: "odlaw", x_coord: 31.78, y_coord: 40.16},
+    wenda: {name: "wenda", x_coord: 48.96, y_coord: 40.26},
+    whitebeard: {name: "whitebeard", x_coord: 7.03, y_coord: 47.82},
+  }, 
+
+  "On the Beach": {
+    waldo: {name: "waldo", x_coord: 62, y_coord: 22.46},
+    odlaw: {name: "odlaw", x_coord: 9.62, y_coord: 21.01},
+    wenda: {name: "wenda", x_coord: 78.15, y_coord: 24.63},
+    whitebeard: {name: "whitebeard", x_coord: 26.5, y_coord: 20.8},
+  }
+}
+
+data.each do |key, value|
+  picture = Picture.create(name: key)
+  picture.characters.create(data[key][:waldo])
+  picture.characters.create(data[key][:odlaw])
+  picture.characters.create(data[key][:wenda])
+  picture.characters.create(data[key][:whitebeard])
+end
