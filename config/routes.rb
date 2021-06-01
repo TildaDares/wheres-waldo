14f031dbe6305a7437a5257b2eb0342fcc67be1f
TildaDares <mathildaudufo@gmail.com>
2021-06-01 14:16:38 +0100

    removed show routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :pictures, only: [:index, :show] do
-        resources :characters, only: [:index, :show]
-        resources :players, only: [:index, :create, :show]
+        resources :characters, only: [:index]
+        resources :players, only: [:index, :create]
       end
     end
   end
