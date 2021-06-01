c84bd02bff542ff1425a77c85cc638ba4ce6b4c2
TildaDares <mathildaudufo@gmail.com>
2021-06-01 13:34:15 +0100

    removed uniqueness atribute

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,5 +1,5 @@
 class Character < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
   validates :x_coord, :y_coord, presence: true
   belongs_to :picture
 end
