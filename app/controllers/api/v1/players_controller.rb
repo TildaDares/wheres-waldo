8673f9c658a6306a821e152a2773178c60da25c3
TildaDares <mathildaudufo@gmail.com>
2021-06-02 20:20:45 +0100

    controller for player

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -1,6 +1,22 @@
 class Api::V1::PlayersController < ApplicationController
   def index
-    players = Player.all
+    picture = Picture.find(params[:picture_id])
+    players = picture.players.all.order(time: :asc)
     render json: players
+  end
+
+  def create
+    player = Player.create!(player_params)
+    if player
+      render json: player
+    else
+      render json: player.errors
+    end
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:name, :time, :picture_id)
   end
 end
